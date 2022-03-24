e0e8be05e8db1ec7c655140ada2e21431deabdf7
MJPEIT <Kirsan.fedorov@tptlive.ee>
2022-03-24 11:21:42 +0200

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,69 @@
+next_position = input("Please type what position: ")
+
+if next_position == "1":
+    print("x . .")
+    print(". . .")
+    print(". . .")
+
+    next_position = input("Please type what position: ")
+
+    if next_position == "1":
+        print("CANT DO THE MOVE")
+
+    elif next_position == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
+
+        next_position = input("Please type what position: ")
+
+        if next_position == "1":
+            print("CANT DO THE MOVE")
+
+        elif next_position == "2":
+            print("CANT DO THE MOVE")
+
+        elif next_position == "3":
+            print("x o x")
+            print(". . .")
+            print(". . .")
+
+elif next_position == "2":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "3":
+    print(". . x")
+    print(". . .")
+    print(". . .")
+
+if next_position == "4":
+    print(". . .")
+    print("x . .")
+    print(". . .")
+
+if next_position == "5":
+    print(". . .")
+    print(". x .")
+    print(". . .")
+
+if next_position == "6":
+    print(". . .")
+    print(". . x")
+    print(". . .")
+
+if next_position == "7":
+    print(". . .")
+    print(". . .")
+    print("x . .")
+
+if next_position == "8":
+    print(". . .")
+    print(". . .")
+    print(". x .")
+
+if next_position == "9":
+    print(". . .")
+    print(". . .")
+    print(". . x")
